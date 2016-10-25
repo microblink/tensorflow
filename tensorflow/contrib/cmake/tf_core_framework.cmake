29ae739015aac6ac5308bf0beda843a4b81818c0
Domagoj Saric <dsaritz@gmail.com>
2016-10-25 16:07:40 +0200

    Quick-fixes for 'highwayhash' build errors. Minor other changes.

--- tensorflow/contrib/cmake/tf_core_framework.cmake
+++ tensorflow/contrib/cmake/tf_core_framework.cmake
@@ -174,7 +174,7 @@ add_dependencies(tf_core_lib ${tensorflow_EXTERNAL_DEPENDENCIES} tf_protos_cc)
 # ${VERSION_INFO_CC} would cache, but it depends on a phony never produced
 # target.
 set(VERSION_INFO_CC ${tensorflow_source_dir}/tensorflow/core/util/version_info.cc)
-add_custom_target(force_rebuild_target DEPENDS ${VERSION_INFO_CC})
+add_custom_target(force_rebuild_target DEPENDS ${VERSION_INFO_CC}) # ALL
 add_custom_command(OUTPUT __force_rebuild COMMAND cmake -E echo)
 add_custom_command(OUTPUT
     ${VERSION_INFO_CC}
